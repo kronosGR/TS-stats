c19fb686032ed7ba0f06512d47b8a907cc02880c
KronosGR <geo.elgeo@gmail.com>
2022-02-11 20:01:33 +0100

    update references

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CsvReader = void 0;
+exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
-const utils_1 = require("./utils");
-class CsvReader {
+class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
         this.data = [];
@@ -19,18 +18,7 @@ class CsvReader {
             .split('\n')
             .map((row) => {
             return row.split(',');
-        })
-            .map((row) => {
-            return [
-                (0, utils_1.dateStringToDate)(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6],
-            ];
         });
     }
 }
-exports.CsvReader = CsvReader;
+exports.CsvFileReader = CsvFileReader;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1].trim() === 'Man United' && match[5].trim() === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (
