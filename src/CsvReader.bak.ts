2124776befe7dc5cb81df2be3f732538fc90f196
KronosGR <geo.elgeo@gmail.com>
2022-02-11 17:42:56 +0100

    added MatchReader class and made abstract CsvReade

--- src/CsvReader.bak.ts
+++ src/CsvReader.bak.ts
@@ -1,16 +1,14 @@
 import fs from 'fs';
 import { MatchResult } from './MatchResult';
+import { dateStringToDate } from './utils';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export abstract class CsvReader {
+export class CsvReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
-  abstract mapRow(row: string[]): MatchData;
-
-  
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -20,7 +18,16 @@ export abstract class CsvReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(this.mapRow);
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
   }
-
 }
