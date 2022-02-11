7aa0d0a01360922a041c316e50449db4f653e380
KronosGR <geo.elgeo@gmail.com>
2022-02-11 11:29:22 +0100

    added trim()

--- build/index.js
+++ build/index.js
@@ -4,9 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('football.csv', {
+const matches = fs_1.default
+    .readFileSync('football.csv', {
     encoding: 'utf-8',
-}).split('\n').map((row) => {
+})
+    .split('\n')
+    .map((row) => {
     return row.split(',');
 });
-console.log(matches);
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1].trim() === 'Man United' && match[5].trim() === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2].trim() === 'Man United' && match[5].trim() === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8',
-}).split('\n').map((row)=> {
-  return row.split(',')
-})
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
 
-console.log(matches)
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (match[1].trim() === 'Man United' && match[5].trim() === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2].trim() === 'Man United' && match[5].trim() === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
