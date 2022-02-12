d81d334646cce44d121b53296a1468f924a075f8
KronosGR <geo.elgeo@gmail.com>
2022-02-12 13:32:02 +0100

    put it all together

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+
+summary.buildAndPrintReport(matchReader.matches)
+
+
