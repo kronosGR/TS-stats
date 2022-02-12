0deb9c551d5e1964506cf102c4e520d6403d7ded
KronosGR <geo.elgeo@gmail.com>
2022-02-12 16:45:43 +0100

    Added HTML report

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
@@ -10,7 +11,9 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
 const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
 
 summary.buildAndPrintReport(matchReader.matches)
+summary2.buildAndPrintReport(matchReader.matches)
 
 
